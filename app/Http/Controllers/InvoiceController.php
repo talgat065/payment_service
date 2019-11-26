042438262b32d090c2c8e2c44b7821b590f6723b
talgat065 <talgat06585@gmail.com>
2019-11-26 12:51:47 +0600

    feat(invoice): add update endpoint

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -21,13 +21,41 @@ class InvoiceController extends Controller
             'payment_time_limit' => 'required|date_format:Y-m-d H:i:s',
         ]);
 
-        app('db')->table('invoices')->insert($request->all());
-
         try {
             app('db')->table('invoices')->insert($request->all());
             return response()->json([
                 'status' => true,
-            ]);
+            ], 201);
+        } catch (QueryException $ex) {
+            // Catch an exception
+        }
+
+        return response()->json([
+            'status' => false,
+        ], 500);
+    }
+
+    /**
+     * @param  Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'id' => 'required|integer',
+            'order_price' => 'required|integer',
+            'payment_time_limit' => 'required|date_format:Y-m-d H:i:s',
+        ]);
+
+        try {
+            app('db')
+                ->table('invoices')
+                ->where('id', $request->get('id'))
+                ->update($request->all());
+            return response()->json([
+                'status' => true,
+            ], 200);
         } catch (QueryException $ex) {
             // Catch an exception
         }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ $router->group(['prefix' => 'api'], function() use ($router) {
     // Private endpoints
     $router->group(['prefix' => 'private'], function() use ($router) {
         $router->post('/invoice/create', 'InvoiceController@store');
+        $router->put('/invoice/update', 'InvoiceController@update');
     });
 });
 
